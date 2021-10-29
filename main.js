8139c8884d624f1c1f1363077b03dc14945ee285
SturmEnte <55847228+SturmEnte@users.noreply.github.com>
2021-10-29 23:13:18 +0200

    added comments and removed console.log's

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const CONFIG = require('./config.json');
 const API_ENDPOINT = 'https://inspirobot.me/api?generate=true';
 
 function sendPoem() {
+    // Fetches a new image url from inspirobot.me
     fetch(API_ENDPOINT)
         .then((res) => {
-            console.log(res);
+            // Formats the response in a way that the url can be used
             res.text()
                 .then((url) => {
-                    console.log(url);
+                    // Sends the image to the webhook which sends it into the channel
                     fetch(CONFIG.webhook, {
                         method: 'post',
                         body: JSON.stringify({
@@ -18,14 +19,14 @@ function sendPoem() {
                         headers: {
                             'Content-Type': 'application/json',
                         },
-                    })
-                        .then(console.log)
-                        .catch(console.error);
+                    }).catch(console.error);
                 })
                 .catch(console.error);
         })
         .catch(console.error);
 }
 
+// Triggers the function on start
 sendPoem();
+// Creates an interval which will trigger the specified function every 5 minutes
 setInterval(sendPoem, 1000 * 60 * 5);
